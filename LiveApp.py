f36a0edc172bd60d131f70bf77183f43905e673a
Roy T.Burns <roy.burns@163.com>
2018-01-24 22:55:49 +0800

    测试数据

--- LiveApp.py
+++ LiveApp.py
@@ -6,16 +6,19 @@
 # Blog：http://this.ispenn.com
 
 import json
-import http.client,mimetypes
+# import http.client
+import requests
+import mimetypes
 from urllib.parse import urlencode
 import random
 import time
 import re
 import logging
-import os,sys
+import os
+import sys
 try:
     import xlrd
-except:
+except ImportError as e:
     os.system('pip install -U xlrd')
     import xlrd
 try:
@@ -31,15 +34,16 @@ from email.mime.text import MIMEText
 log_file = os.path.join(os.getcwd(),'log/liveappapi.log')
 log_format = '[%(asctime)s] [%(levelname)s] %(message)s'
 logging.basicConfig(format=log_format,filename=log_file,filemode='w',level=logging.DEBUG)
-console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
+# console = logging.StreamHandler()
+# console.setLevel(logging.DEBUG)
 formatter = logging.Formatter(log_format)
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
+# console.setFormatter(formatter)
+# logging.getLogger('').addHandler(console)
 
 #获取并执行测试用例
 def runTest(testCaseFile):
-    testCaseFile = os.path.join(os.getcwd(),testCaseFile)
+    testCaseFile = os.path.join(os.getcwd(), testCaseFile)
+    print(testCaseFile)
     if not os.path.exists(testCaseFile):
         logging.error('测试用例文件不存在！！！')
         sys.exit()
@@ -49,7 +53,7 @@ def runTest(testCaseFile):
     correlationDict = {}
     correlationDict['${hashPassword}'] = hash1Encode('123456')
     correlationDict['${session}'] = None
-    for i in range(1,table.nrows):
+    for i in range(1, table.nrows):
         correlationDict['${randomEmail}'] = ''.join(random.sample('abcdefghijklmnopqrstuvwxyz',6)) + '@automation.test'
         correlationDict['${randomTel}'] = '186' + str(random.randint(10000000,99999999))
         correlationDict['${timestamp}'] = int(time.time())
@@ -69,6 +73,7 @@ def runTest(testCaseFile):
             if request_url.find(key) > 0:
                 request_url = request_url.replace(key,str(correlationDict[key]))
         if request_data_type == 'Form':
+            print(request_data)
             dataFile = request_data
             if os.path.exists(dataFile):
                 fopen = open(dataFile,encoding='utf-8')
@@ -88,9 +93,10 @@ def runTest(testCaseFile):
                     request_data = urlencode(request_data).replace("%27","%22")
                 elif encryption == 'DES':
                     request_data = json.loads(request_data)
-                    request_data = urlencode({'param':encodePostStr(request_data)})
+                    request_data = urlencode({'param': encodePostStr(request_data)})
                 else:
-                    request_data = urlencode(json.loads(request_data))
+                    # request_data = urlencode(json.loads(request_data))
+                    request_data = json.loads(request_data)
             except Exception as e:
                 logging.error(num + ' ' + api_purpose + ' 请求的数据有误，请检查[Request Data]字段是否是标准的json格式字符串！')
                 continue
@@ -109,7 +115,7 @@ def runTest(testCaseFile):
             if not os.path.exists(dataFile):
                 logging.error(num + ' ' + api_purpose + ' 文件路径配置无效，请检查[Request Data]字段配置的文件路径是否存在！！！')
                 continue
-            fopen = open(dataFile,'rb')
+            fopen = open(dataFile, 'rb')
             data = fopen.read()
             fopen.close()
             request_data = '''
@@ -120,18 +126,22 @@ Content-Transfer-Encoding:binary
 
 %s
 ------WebKitFormBoundaryDf9uRfwb8uzv1eNe--
-    ''' % (os.path.basename(dataFile),data)
-        status,resp = interfaceTest(num,api_purpose,api_host,request_url,request_data,check_point,request_method,request_data_type,correlationDict['${session}'])
-        if status != 200:
-            errorCase.append((num + ' ' + api_purpose,str(status),'http://'+api_host+request_url,resp))
-            continue
+    ''' % (os.path.basename(dataFile), data)
+
+        status, reason = interfaceTest(num, api_purpose, api_host, request_url, request_data, check_point,
+                                     request_method, request_data_type, correlationDict['${session}'])
+
+        errorCase.append((num + ' ' + api_purpose, str(status), api_host + request_url, reason))
+        # if status != 200:
+        #     errorCase.append((num + ' ' + api_purpose, str(status), api_host + request_url, reason))
+        #     continue
         for j in range(len(correlation)):
             param = correlation[j].split('=')
             if len(param) == 2:
-                if param[1] == '' or not re.search(r'^\[',param[1]) or not re.search(r'\]$',param[1]):
+                if param[1] == '' or not re.search(r'^\[', param[1]) or not re.search(r'\]$', param[1]):
                     logging.error(num + ' ' + api_purpose + ' 关联参数设置有误，请检查[Correlation]字段参数格式是否正确！！！')
                     continue
-                value = resp
+                value = reason
                 for key in param[1][1:-1].split(']['):
                     try:
                         temp = value[int(key)]
@@ -146,11 +156,11 @@ Content-Transfer-Encoding:binary
 
 # 接口测试
 def interfaceTest(num,api_purpose,api_host,request_url,request_data,check_point,request_method,request_data_type,session):  
-    headers = {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-               'X-Requested-With':'XMLHttpRequest',
-               'Connection':'keep-alive',
-               'Referer':'http://' + api_host,
-               'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+               'X-Requested-With': 'XMLHttpRequest',
+               'Connection': 'keep-alive',
+               'Referer': 'http://' + api_host,
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'}
     if session is not None:
         headers['Cookie'] = 'session=' + session
         if request_data_type == 'File':
@@ -158,28 +168,37 @@ def interfaceTest(num,api_purpose,api_host,request_url,request_data,check_point,
         elif request_data_type == 'Data':
             headers['Content-Type'] = 'text/plain; charset=UTF-8'
 
-    conn = http.client.HTTPConnection(api_host)
-    if request_method == 'POST':
-        conn.request('POST',request_url,request_data,headers=headers)
-    elif request_method == 'GET':
-        conn.request('GET',request_url+'?'+request_data,headers=headers)
-    else:
-        logging.error(num + ' ' + api_purpose + ' HTTP请求方法错误，请确认[Request Method]字段是否正确！！！')
-        return 400,request_method
-    response = conn.getresponse()
-    status = response.status
-    resp = response.read()
+    # conn = http.client.HTTPConnection(api_host)
+    # if request_method == 'POST':
+    #     conn.request('POST', request_url, request_data, headers=headers)
+    # elif request_method == 'GET':
+    #     conn.request('GET', request_url+'?'+request_data, headers=headers)
+    # else:
+    #     logging.error(num + ' ' + api_purpose + ' HTTP请求方法错误，请确认[Request Method]字段是否正确！！！')
+    #     return 400, request_method
+    # response = conn.getresponse()
+    # status = response.status
+    # resp = response.read()
+
+    print(api_host + request_url)
+    print(request_data)
+    resp = requests.post(api_host + request_url, json=request_data, headers=headers)
+    status = resp.status_code
+    reason = resp.reason
+
     if status == 200:
-        resp = resp.decode('utf-8')
-        if re.search(check_point,str(resp)):
-            logging.info(num + ' ' + api_purpose + ' 成功, ' + str(status) + ', ' + str(resp))
-            return status,json.loads(resp)
-        else:
-            logging.error(num + ' ' + api_purpose + ' 失败！！！, [ ' + str(status) + ' ], ' + str(resp))
-            return 2001,resp
+        # resp = resp.decode('utf-8')
+        logging.info(num + ', ' + api_purpose + ', ' + str(status) + ', ' + str(reason))
+        # if re.search(check_point, str(resp)):
+        #     logging.info(num + ', ' + api_purpose + ', ' + str(status) + ', ' + str(resp))
+        #     return status, json.loads(resp)
+        # else:
+        #     logging.error(num + ' ' + api_purpose + ', ' + str(status) + ', ' + str(resp))
+        #     return 2001, resp
     else:
-        logging.error(num + ' ' + api_purpose + ' 失败！！！, [ ' + str(status) + ' ], ' + str(resp))
-        return status,resp.decode('utf-8')
+        logging.error(num + ' ' + api_purpose + ', ' + str(status) + ', ' + str(reason))
+        # return status, resp.decode('utf-8')
+    return status, reason
 
 #获取md5验证码
 def getMD5(url,postData):
@@ -204,7 +223,7 @@ def desEncode(desStr):
 
 # 字典排序
 def encodePostStr(postData):
-    keyDict = {'key':'secretKEY'}
+    keyDict = {'key': 'secretKEY'}
     mergeDict = dict(postData, **keyDict)
     mergeDict = sorted(mergeDict.items())
     postStr = ''
@@ -217,7 +236,7 @@ def encodePostStr(postData):
     postData['token'] = token
     return desEncode(postData)
 
-#发送通知邮件
+# 发送通知邮件
 def sendMail(text):
     sender = 'no-reply@myhost.cn'  
     receiver = ['penn@myhost.cn']
@@ -238,14 +257,21 @@ def sendMail(text):
     smtp.sendmail(sender, receiver + mailToCc, msg.as_string())  
     smtp.quit() 
 
+
 def main():
-    errorTest = runTest('TestCase/TestCasePre.xlsx')
+    errorTest = runTest('TestCase/TestCase.xlsx')
     if len(errorTest) > 0:
         html = '<html><body>接口自动化定期扫描，共有 ' + str(len(errorTest)) + ' 个异常接口，列表如下：' + '</p><table><tr><th style="width:100px;">接口</th><th style="width:50px;">状态</th><th style="width:200px;">接口地址</th><th>接口返回值</th></tr>'
         for test in errorTest:
+            print(test[0])
+            print(test[1])
+            print(test[2])
+            print(test[3])
             html = html + '<tr><td>' + test[0] + '</td><td>' + test[1] + '</td><td>' + test[2] + '</td><td>' + test[3] + '</td></tr>'
+
         html = html + '</table></body></html>'
         #sendMail(html)
+
 
 if __name__ == '__main__':
     main()
